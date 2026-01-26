7ea2c3dee43bcd9144d24996c5e52b1a7cfad63e
Toaster12321 <tonsterrocks123@gmail.com>
2026-01-25 18:43:44 -0800

    Trees,Bushes, Dashing + SFX
    added new normal maps and collision for trees and bushes
    switched from fade -> cutout for materials
    populated map with more trees/bushes
    added dash mechanic
    added footsteps and out of breath SFX

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,10 +4,16 @@ using UnityEngine.InputSystem;
 public class FirstPersonController : MonoBehaviour
 {
     public bool CanMove {  get; private set; } = true; //can the player move
+    public bool isDashing = false;
+    public bool dashCoolingDown = false;
 
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float dashCooldown = 4f;
+    [SerializeField] private float dashDuration = 6f;
     [SerializeField] private float gravity = 30.0f;
+    [SerializeField] private AudioSource walking;
+    [SerializeField] private AudioSource outOfBreath;
 
     [Header("Look Parameters")]
     [SerializeField, Range(0.1f, 10)] private float lookSpeedX = 2.0f;//speed at which the camera moves in x and y directions
@@ -23,6 +29,8 @@ public class FirstPersonController : MonoBehaviour
     private Vector2 cameraInput;
 
     private float rotationX = 0;
+    private float dashTime = 0f;
+    private float lastDashTime = -Mathf.Infinity;
 
     [SerializeField] private InputActionReference moveInput; //references for input system controls
     [SerializeField] private InputActionReference lookInput;
@@ -51,15 +59,41 @@ public class FirstPersonController : MonoBehaviour
         {   
             HandleMovementInput();
             HandleMouseLook();
-
             ApplyFinalMovement();
+            footstepsWalking();
         }
         
+        if(isDashing) //start dash timer
+            dashTime += Time.deltaTime;
+
+        if (Time.time >= lastDashTime + dashCooldown)//reset cooldown for dash
+            dashCoolingDown = false;
     }
 
     private void HandleMovementInput()
     {
-        currentInput = moveInput.action.ReadValue<Vector2>() * walkSpeed; //reads up,down,left,right movement for walk speed
+        currentInput = (moveInput.action.ReadValue<Vector2>()).normalized * walkSpeed; //reads up,down,left,right movement for walk speed (normalize so diag movement isnt faster)
+        if (Keyboard.current.shiftKey.wasPressedThisFrame && isDashing == false && !dashCoolingDown && currentInput != new Vector2(0,0))//while shift is held, dash cooldown is over and we are moving a direction
+        {
+            walkSpeed *= 2; //press shift to double move speed
+            isDashing = true;
+        }
+
+        if (Keyboard.current.shiftKey.wasReleasedThisFrame && isDashing == true) 
+        {
+            dashTime = 0f; //reset dash time and move speed
+            walkSpeed /= 2;
+            isDashing = false;
+        }
+        else if (dashTime >= dashDuration)
+        {
+            outOfBreath.Play(); //play SFX
+            dashCoolingDown = true;
+            dashTime = 0f; //reset dash time and move speed
+            walkSpeed /= 2;
+            lastDashTime = Time.time; //store last dashed for 4s cooldown
+            isDashing = false;
+        }
 
         float moveDirectionY = moveDirection.y;
         moveDirection = (transform.forward * currentInput.y) + (transform.right * currentInput.x); //sets movedirection.y to our forward direction based on left/right movement and the right direction based on the up/down movement
@@ -84,6 +118,17 @@ public class FirstPersonController : MonoBehaviour
             moveDirection.y -= gravity * Time.deltaTime; //if we are in the sky appply gravity each frame
 
         characterController.Move(moveDirection * Time.deltaTime);//move the character controller in a direction each frame
+    }
+
+    private void footstepsWalking()
+    {
+        bool isMoving = 
+            currentInput != new Vector2(0, 0); //bool for movement 
+
+        if (isMoving && !walking.isPlaying) //play footsteps when walking and not dashing
+            walking.Play();
+        else if (!isMoving || isDashing == true)
+            walking.Stop();
     }
 
     private void OnEnable() //enable controls
