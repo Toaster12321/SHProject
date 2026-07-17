238b9cf8382edbf853d7e77afbf4d36fd6bd299b
Toaster12321 <tonsterrocks123@gmail.com>
2026-07-16 21:43:54 -0700

    Spider Jump Fixes
    the spider's attack state now 1. rotates towards player, 2. starts animation for wind up, 3. begins jump, 4. lands at an end position 5. switch to chase state
    6.repeat
    
    the spider now checks for obstacles mid jump to prevent clipping into walls or trees
    spider no longer slides at the end of jump
    spider no longer switches to idle before the attack animation is triggered during the yield window

--- Assets/Scripts/Enemy/EnemyStateAttack.cs
+++ Assets/Scripts/Enemy/EnemyStateAttack.cs
@@ -7,36 +7,47 @@ public class EnemyStateAttack : EnemyState
     private float _attackCooldown = 2f;//how long before damage is applied again
     private float _lastAttackTime;
     private float _rotationSpeed = 150f;
+    private float _rotationTolerance = 3f;
+    private Quaternion targetRotation;
 
-    private float jumpHeight = 1.2f;
+    //spider jump variables
+    private float jumpHeight = 2f;
     private float jumpDisplacement = 7f;
-    private float jumpDuration = 0.8f;
+    private float jumpDuration = 0.52f;
 
     private bool _jumpFinished = false;
+    private bool _rotatedTowardsPlayer = false;
+    private bool _hasntAttackedYet = true;
+
+    private LayerMask obstacleLayerMask = LayerMask.GetMask("WhatIsWall", "Shrubbery"); //cant jump over walls or shrubbery
     public EnemyStateAttack( EnemyStateContext context, EnemyStateMachine.EEnemyState estate) : base(context, estate)
     {
-        
     }
 
     public override void EnterState()
     {
         _lastAttackTime = 0f;
-        Context.Animator.SetBool("attacking", true);
+
+        _hasntAttackedYet = true; //first time entering the state, there have been no attacks
         _jumpFinished = false;
+        _rotatedTowardsPlayer = false;
 
         if (Context.EnemyType == EnemyStateMachine.EnemyType.Scab)
         {
             Context.Agent.SetDestination(Context.SelfTransform.position);
+            Context.Animator.SetBool("attacking", true);
         }
         
         if (Context.EnemyType == EnemyStateMachine.EnemyType.CarnPlant)
         {
             Context.IsRotatingEnabled = true;
+            Context.Animator.SetBool("attacking", true);
         }
 
         if (Context.EnemyType == EnemyStateMachine.EnemyType.MushroomSpider)
         {
             Context.EnemyStateMachine.StartCoroutine(JumpAttack());
+            Context.Animator.SetBool("inAttack", true); //prevents switching back to idle via chase state
         }
 
     }
@@ -44,7 +55,8 @@ public class EnemyStateAttack : EnemyState
     public override void ExitState()
     {
         if (Context.EnemyType == EnemyStateMachine.EnemyType.Scab || Context.EnemyType == EnemyStateMachine.EnemyType.MushroomSpider)
-            Context.Agent.isStopped = false;
+            Context.Agent.isStopped = false; //allow agent movement again
+
         Context.Animator.SetBool("attacking", false);
     }
 
@@ -52,10 +64,20 @@ public class EnemyStateAttack : EnemyState
     {
         if (Context.EnemyType == EnemyStateMachine.EnemyType.CarnPlant && Context.IsRotatingEnabled)
         {
-            var lookPos = Context.Player.position - Context.SelfTransform.position; //vector from enemy to player
-            lookPos.y = 0; //ignore vertical rotation, only moving along y axis
-            var lookRotation = Quaternion.LookRotation(lookPos); //turns Vector3 -> Quaternion with rotation
-            Context.SelfTransform.rotation = Quaternion.RotateTowards(Context.SelfTransform.rotation, lookRotation, _rotationSpeed * Time.deltaTime); //set rotation to rotate towards the player at set rotation speed 
+            RotateTowardsPlayer();
+        }
+
+        if((Context.EnemyType == EnemyStateMachine.EnemyType.MushroomSpider && _jumpFinished) || (_hasntAttackedYet)) 
+        {
+            //if the jump is finished or its the 1st time attacking, rotate to the player facing him head on
+            RotateTowardsPlayer();
+
+            //if the angle between the player and spider is less than the rotation tolerance(3f) we have rotated towards the player
+            float angleBetweenPlayer = Quaternion.Angle(Context.SelfTransform.rotation, targetRotation);
+            if (angleBetweenPlayer <= _rotationTolerance)
+                _rotatedTowardsPlayer = true;
+            else
+                _rotatedTowardsPlayer = false;
         }
 
     }
@@ -71,10 +93,8 @@ public class EnemyStateAttack : EnemyState
 
 
             if (Context.EnemyType == EnemyStateMachine.EnemyType.MushroomSpider)
-                if (Context.PlayerInSightRange && !Context.PlayerInAttackRange && _jumpFinished) //if player enters vision radius -> chase
+                if (_jumpFinished) //switch back to chase state after each jump attack
                     return EnemyStateMachine.EEnemyState.Chase;
-                else if (!Context.PlayerInSightRange && !Context.PlayerInAttackRange && _jumpFinished)
-                    return EnemyStateMachine.EEnemyState.Patrol;
 
         }
         else if (Context.EnemyType == EnemyStateMachine.EnemyType.CarnPlant)
@@ -102,7 +122,7 @@ public class EnemyStateAttack : EnemyState
         if (_player == null) //do nothing if not player collider
             return;
 
-        if (Time.time >= _lastAttackTime + _attackCooldown)
+        if (Time.time >= _lastAttackTime + _attackCooldown) //if attack cooldown is over
         {
             Debug.Log("take dmg");
             Context.Player.GetComponentInParent<Player>().TakeDamage(1); //apply damage
@@ -112,41 +132,66 @@ public class EnemyStateAttack : EnemyState
 
     }
 
+    private void RotateTowardsPlayer()
+    {
+        var targetPos = Context.Player.position - Context.SelfTransform.position; //vector from enemy to player
+        targetPos.y = 0; //ignore vertical rotation, only moving along y axis
+        targetRotation = Quaternion.LookRotation(targetPos); //turns Vector3 -> Quaternion with rotation
+        Context.SelfTransform.rotation = Quaternion.RotateTowards(Context.SelfTransform.rotation, targetRotation, _rotationSpeed * Time.deltaTime); //set rotation to rotate towards the player at set rotation speed 
+    }
+
     private IEnumerator JumpAttack()
     {
+        yield return new WaitUntil(() => _rotatedTowardsPlayer); //wait till spider has rotated towards player
+        Context.Animator.SetBool("attacking", true); //start attacking animation 
+        _hasntAttackedYet = false;
+
         Vector3 _startPos = Context.SelfTransform.position;
-        Vector3 _endPos = _startPos + Context.SelfTransform.forward * jumpDisplacement;
+        Vector3 _endPos = _startPos + Context.SelfTransform.forward * jumpDisplacement; //end pos is the current position + the z-forward direction * the total displacement we want it to jump to
+
+        Vector3 capsuleBot = _startPos + Vector3.up * 0.2f; //capsule cast sizes
+        Vector3 capsuleTop = _startPos + Vector3.up * 1.9f;
+
+        Vector3 direction = (_endPos - _startPos).normalized; //get the current direction the spider is facing
+        float totalDistance = Vector3.Distance(_startPos, _endPos);
+
+        //CapsuleCast aroudn the spider to check if it hit an obstacle in its radius for the whole distance of the jump
+        if (Physics.CapsuleCast(capsuleBot, capsuleTop, Context.Agent.radius, direction, out RaycastHit hitInfo, totalDistance, obstacleLayerMask))
+        {
+            _endPos = hitInfo.point - direction * 0.5f; //if an objstacle is hit change the end position to half of the area where the collision occured
+        }
 
         float elapsedTime = 0f;
-        Context.Agent.isStopped = true;
+        Context.Agent.isStopped = true; //stop all agent functions
         Context.Agent.updatePosition = false;
         Context.Agent.updateRotation = false;
 
-        yield return new WaitForSeconds(0.5f);
+        float _timeTillJump = 0.5f;
+        yield return new WaitForSeconds(_timeTillJump); 
         
-        while (elapsedTime < jumpDuration)
+        while (elapsedTime < jumpDuration) //during the jump duration
         {
             elapsedTime += Time.deltaTime;
-            float currentTime = elapsedTime / jumpDuration;
+            float currentTime = elapsedTime / jumpDuration; //convert time to a 0-100% percentage
 
-            Vector3 _pos = Vector3.Lerp(_startPos, _endPos, currentTime); //forward movement
+            Vector3 _pos = Vector3.Lerp(_startPos, _endPos, currentTime); //move in a direction from start to end during the time window
 
-            _pos.y += Mathf.Sin(currentTime * MathF.PI) * jumpHeight;
+            _pos.y += Mathf.Sin(currentTime * MathF.PI) * jumpHeight; //sin creates a wave to simulate jumping, PI = 180degrees, so from 0 to 180degrees in a wave we are moving in the y axis
 
-            Context.SelfTransform.position = _pos;
+            Context.SelfTransform.position = _pos; //set spider position to the lerp
 
-            yield return null;
+            yield return null; //dont move on till jump is over
         }
 
-        Context.SelfTransform.position = _endPos;
+        Context.SelfTransform.position = _endPos; //set agent and current position to end location
         Context.Agent.Warp(_endPos);
 
-        Context.Agent.updatePosition = true;
+        Context.Agent.updatePosition = true; //resume agent functions
         Context.Agent.updateRotation = true;
         Context.Agent.isStopped = false;
 
         _jumpFinished = true;
-
+        Context.Animator.SetBool("inAttack", false);
     }
 
 }
