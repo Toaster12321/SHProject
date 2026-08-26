8e00a6a8d26aa975b55f47f873d7e19ac1158e96
Toaster12321 <tonsterrocks123@gmail.com>
2026-08-25 21:37:55 -0700

    Dialogue System
    new dialogue system implemented
    text is displayed in a text box alongside a nameplate of the character talking in the bottom center of the UI
    text is shown one letter at a time unless the continue button is pressed then the text will finish immediately
    dialogue is triggered currently via collision player detection
    
    need to stop player movement and change trigger to include on interaction

--- Assets/Scripts/CutsceneActivator.cs
+++ Assets/Scripts/CutsceneActivator.cs
@@ -1,8 +1,28 @@
-using NUnit.Framework;
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class CutsceneActivator : MonoBehaviour
 {
     [SerializeField] private List<DialogueLines> dialogueLines;
+    [SerializeField] private CutsceneManager cutsceneManager;
+    [SerializeField] private bool playOnlyOnce = true;
+
+    private bool hasPlayedOnce = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player _player = other.GetComponent<Player>();
+        if (_player == null)
+            return;
+
+        if (playOnlyOnce && hasPlayedOnce)
+            return;
+
+        if (cutsceneManager != null )
+        {
+            hasPlayedOnce = true;
+            cutsceneManager.StartCutscene(dialogueLines);
+        }
+    }
 }
---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,118 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering.LookDev;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CutsceneManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private GameObject dialogueUI;
+    [SerializeField] private TMP_Text characterNamePlate;
+    [SerializeField] private TMP_Text dialogueText;
+
+    private float charsPerSecond = 40f;
+
+    private List<DialogueLines> currentLines;
+    private int currentLineIndex;
+    private Coroutine typingRoutine;
+
+    private InputAction continueAction;
+    private bool textActive = false;
+    private bool cutsceneActive = false;
+    private string fullLineText;
+
+
+    private void Start()
+    {
+        continueAction = FirstPersonController.playerInput.actions["Interact"];
+        dialogueUI.SetActive(false); //hide UI
+    }
+
+    private void Update()
+    {
+        if (!cutsceneActive)
+            continueAction.performed -= ContinueDialogue;
+        else
+            continueAction.performed += ContinueDialogue;
+    }
+
+    public void StartCutscene(List<DialogueLines> dialogueLines)
+    {
+        if (dialogueLines == null || dialogueLines.Count == 0)
+            return;
+
+        currentLines = dialogueLines;
+        currentLineIndex = 0;
+        cutsceneActive = true;
+
+        dialogueUI.SetActive(true);
+        ShowCurrentLine();
+    }
+
+    private void ShowCurrentLine()
+    {
+        DialogueLines currentDialogue = currentLines[currentLineIndex];
+
+        characterNamePlate.text = currentDialogue.characterName.ToString();
+        fullLineText = currentDialogue.lineText;
+
+        if (typingRoutine != null)
+            StopCoroutine(typingRoutine);
+
+        typingRoutine = StartCoroutine(TypeLine());
+    }
+
+    private IEnumerator TypeLine()
+    {
+        textActive = true;
+        dialogueText.text = "";
+
+        foreach (char character in fullLineText)
+        {
+            dialogueText.text += character;
+            yield return new WaitForSeconds(1f / charsPerSecond);
+        }
+
+        textActive = false;
+        typingRoutine = null;
+    }
+
+    private void ContinueDialogue(InputAction.CallbackContext ctx)
+    {
+        if(textActive)
+        {
+            FinishDialogueLine();
+            return;
+        }
+
+        currentLineIndex++;
+
+        if (currentLineIndex >= currentLines.Count)
+            EndCutscene();
+        else
+            ShowCurrentLine();
+
+    }
+
+    private void FinishDialogueLine()
+    {
+        if (typingRoutine != null)
+            StopCoroutine(typingRoutine);
+
+        dialogueText.text = fullLineText;
+        textActive = false;
+        typingRoutine = null;
+    }
+
+
+    private void EndCutscene()
+    {
+        cutsceneActive = false;
+        currentLines = null;
+        dialogueUI.SetActive(false);
+    }
+}
