334552803eb52d3a8fe6ad4f778746c31c4830ef
Toaster12321 <tonsterrocks123@gmail.com>
2026-09-14 21:27:22 -0700

    Title Screen
    added title screen with UI + buttons
    added sfx to button hover/switch
    name for game added
    5 options: new game, load game, options, credits, exit game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
